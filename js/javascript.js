b38763db2b21244700d37a4e5d41a4959e1544bd
Yask! <yaskarawaiedly@hotmail.com>
2024-11-20 19:31:36 -0300

    criaçao do Javascript

---
+++ js/javascript.js
@@ -0,0 +1,18 @@
+window.addEventListener('load', function(event) {
+
+    
+    
+    var btn_calcular = document.getElementById("btn_calcular");
+
+    btn_calcular.addEventListener('click', function(e) {
+
+        var campo_a = document.getElementById ('campo_a');
+        var campo_b = document.getElementById ('campo_b');
+        var campo_C = document.getElementById ('campo_c');
+
+        var resultado = parseInt(campo_a.value) + parseInt(campo_b.value);
+        campo_C.value = resultado
+        
+    })
+
+});
